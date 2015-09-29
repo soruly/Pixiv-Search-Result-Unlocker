aa13b2ed4750209cfb5f947181b8be8e9520e77c
soruly <soruly@gmail.com>
2015-09-29 19:00:55 +0800

    Added support for Tampermonkey

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Pixiv-Search-Result-Unlocker
 Unlock some search result in Pixiv, allows you to click on blocked pictures
+Support Tampermonkey on Chrome
--- pixiv-search-unlock.js
+++ pixiv-search-unlock.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Pixiv Search Result Unlocker
-// @namespace    http://your.homepage/
+// @namespace    
 // @version      0.1
 // @description  Unlock some search result in Pixiv, allows you to click on blocked pictures
 // @author       You
