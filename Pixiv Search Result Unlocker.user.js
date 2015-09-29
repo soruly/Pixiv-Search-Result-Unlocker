9d797d491c339e7dd6463998e13a93af15d00ce9
soruly <soruly@gmail.com>
2015-09-29 18:53:56 +0800

    Unlock popular-introduction, Unlock sample-user-search

---
+++ Pixiv Search Result Unlocker.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Pixiv Search Result Unlocker
+// @namespace    http://your.homepage/
+// @version      0.1
+// @description  Unlock some search result in Pixiv, allows you to click on blocked pictures
+// @author       You
+// @match        http://www.pixiv.net/search.php?*
+// @grant        none
+// ==/UserScript==
+
+document.querySelector("section.popular-introduction>a").style.display = 'none';
+document.querySelector("a.premium-link").style.display = 'none';
+document.querySelector(".sample-user-search").classList.remove("disabled");
