a4da921177d9bd4195674d33df8486f374a8fb38
soruly <soruly@gmail.com>
2017-09-22 00:16:23 +0800

    Release version 0.3

--- pixiv-search-unlock.user.js
+++ pixiv-search-unlock.user.js
@@ -1,13 +1,12 @@
 // ==UserScript==
 // @name         Pixiv Search Result Unlocker
 // @namespace    https://github.com/soruly/Pixiv-Search-Result-Unlocker
-// @version      0.2
+// @version      0.3
 // @description  Unlock some search result in Pixiv, allows you to click on blocked pictures
 // @author       soruly
 // @match        *://www.pixiv.net/search.php?*
 // @grant        none
 // ==/UserScript==
 
-document.querySelector("div.popular-introduction-overlay").style.display = "none";
-document.querySelector("a.upload.ad-printservice.hover-item").style.display = "none";
-document.querySelector(".sample-user-search").classList.remove("disabled");
+document.querySelector("div.popular-introduction-overlay").style.display = "none"; // Remove popular section overly blocker
+document.querySelector("a.upload.ad-printservice.hover-item").style.display = "none"; // Remove pixiv premium ads
