0918a2fca07cda8d476b32d2fbaa499196f371fc
soruly <soruly@gmail.com>
2017-10-22 21:05:17 +0800

    Supress error on Pixiv Search Result Unlocker

--- pixiv-search-unlock.user.js
+++ pixiv-search-unlock.user.js
@@ -1,12 +1,16 @@
 // ==UserScript==
 // @name         Pixiv Search Result Unlocker
 // @namespace    https://github.com/soruly/Pixiv-Search-Result-Unlocker
-// @version      0.3
+// @version      0.4
 // @description  Unlock some search result in Pixiv, allows you to click on blocked pictures
 // @author       soruly
 // @match        *://www.pixiv.net/search.php?*
 // @grant        none
 // ==/UserScript==
 
-document.querySelector("div.popular-introduction-overlay").style.display = "none"; // Remove popular section overly blocker
-document.querySelector("a.upload.ad-printservice.hover-item").style.display = "none"; // Remove pixiv premium ads
+if(document.querySelector("div.popular-introduction-overlay")){
+  document.querySelector("div.popular-introduction-overlay").style.display = "none"; // Remove popular section overly blocker
+}
+if(document.querySelector("a.upload.ad-printservice.hover-item")){
+  document.querySelector("a.upload.ad-printservice.hover-item").style.display = "none"; // Remove pixiv premium ads
+}
